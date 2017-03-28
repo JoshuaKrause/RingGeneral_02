eef511c940a72a7f4956f8503af8f2593d64b8c1
JoshuaKrause <jkrause@joshuakrause.net>
2017-03-28 17:50:45 -0400

    Initial commit.

---
+++ RingGeneral/RingGeneral/DataControl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace RingGeneral
+{
+    static class DataControl
+    {
+        const string dataPath = @"C:\Users\jkrau\Desktop\ringGeneralData.txt";
+
+        public static Dictionary<string, Character> characters = new Dictionary<string, Character>();
+        public static Dictionary<string, Tag> tags = new Dictionary<string, Tag>();
+
+        static public void DataReader(string path = null)
+        {
+            if (string.IsNullOrEmpty(path))
+                path = dataPath;
+
+            FileStream fileStream = File.OpenRead(path);
+            StreamReader reader = new StreamReader(fileStream);
+
+            string rawData = reader.ReadToEnd();
+            reader.Close();
+
+            Console.WriteLine(rawData);
+        }
+    }
+}
---
+++ RingGeneral/RingGeneral/Tag.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RingGeneral
+{
+    class Tag : GameObject
+    {
+        public string Name { get; set; }
+    }
+}
